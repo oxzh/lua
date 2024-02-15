85892ddf7d9f8d47c9797b031c63b39b2d641064
oxzh <gabriel_alcantara12@outlook.com>
2024-02-15 18:47:31 -0300

    comparation

---
+++ Aula-01/comp.lua
@@ -0,0 +1,8 @@
+print(25>5)
+--returns boolean true, bcs 25 is > then 5 so, its true.
+
+print(25>=5)
+--same as before, 25 is greater than 5 or equals 5, so its true.
+
+print(5>=25)
+--5 is less than 25 so returns true.
