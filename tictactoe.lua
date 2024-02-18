3263d228a0a54ca5c46bd35ae4d9c6f60ca028c3
oxzh <gabriel_alcantara12@outlook.com>
2024-02-18 18:40:37 -0300

    learning about "\n" to break line and using to print the result

--- tictactoe.lua
+++ tictactoe.lua
@@ -5,7 +5,7 @@ game = {
 }
 
 
-
+--[[ old code
 
 print(
     game[1],  game[4],  game[7]
@@ -17,16 +17,16 @@ print(
     game[3],  game[6],  game[9]
 )
 
-
+]]--
 
 --prints normal if have ONE array, but using two, i cant print for some reason; a = (game[1], game[2]);  a = (game[1];  game[2])
-a = game[1] .. "   ".. game[2] .. "   " .. game[3]
-
-print(a)
-
-
+a = game[1] .. "   ".. game[4] .. "   " .. game[7]
+b = game[2] .. "   ".. game[5] .. "   " .. game[8]
+c = game[3] .. "   ".. game[6] .. "   " .. game[9]
 
 
+
+print("\n", a, "\n", b, "\n", c )
 
 
 
