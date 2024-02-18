be4d49d15a3334fda6fa306dee2dfad1326e372c
oxzh <gabriel_alcantara12@outlook.com>
2024-02-18 16:51:44 -0300

    trying to create tictactoe game in lua using arrays

---
+++ tictactoe.lua
@@ -0,0 +1,36 @@
+game = {
+    [1] = "0",  [4] = "0",  [7] = "0",
+    [2] = "0",  [5] = "X",  [8] = "0",
+    [3] = "0",  [6] = "0",  [9] = "0"
+}
+
+
+
+
+print(
+    game[1],  game[4],  game[7]
+)
+print(
+    game[2],  game[5],  game[8]
+)
+print(
+    game[3],  game[6],  game[9]
+)
+
+
+
+--prints normal if have ONE array, but using two, i cant print for some reason; a = (game[1], game[2]);  a = (game[1];  game[2])
+a = game[1], game[4]
+print(a)
+
+
+
+
+
+
+
+--[[
+game[1]  game[4] game[7]
+game[2]  game[5]  game[8]
+game[3]  game[6]  game[9]
+]]--
