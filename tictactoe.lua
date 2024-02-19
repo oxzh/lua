8cc5a762ab19577c8fa40655e8bbaf0b48ecf9d8
oxzh <gabriel_alcantara12@outlook.com>
2024-02-18 22:26:56 -0300

    tictactoe game

--- tictactoe.lua
+++ tictactoe.lua
@@ -34,14 +34,13 @@ os.execute("sleep 1")
 os.execute("clear")
 
 
-
 --print("Tic Tac Toe Game:", "\n", a, "\n", b, "\n", c )
 
 
 print("O jogador Um ficará com o número 3, e o jogador número Dois ficará com o número 6")
 --os.execute("sleep 2")
 
-print("A forma que está o jogo é de cima para baixo ou seja, 1, 2 e 3 são o primeiro da esquerda, segundo e terceiro.")
+print("A forma que está o jogo é da esquerda para a direita. 1, 2, 3...")
 --os.execute("sleep 2")
 print(" e por aí vai.")
 --os.execute("sleep 2")
@@ -85,6 +84,7 @@ end
 
 print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
 
+os.execute("clear")
 
 io.write("Agora O jogador Dois escolha um local para por seu número: ", "\n",
 print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
@@ -119,10 +119,162 @@ elseif numlocalX == "8" then
 elseif numlocalX == "9" then
     i = 6
 
+ end
+
+ os.execute("clear")
+
+
+ io.write("Agora O jogador Um escolha um local para por seu número: ", "\n",
+print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
+)
+
+numlocalX = io.read()
+if numlocalX == "1" then
+    a = 3
+
+elseif numlocalX == "2" then
+    b = 3
+
+elseif numlocalX  == "3" then
+    c = 3
+
+elseif numlocalX == "4" then
+    d = 3
+
+elseif numlocalX == "5" then
+    e = 3
+
+elseif numlocalX == "6" then
+    f = 3
+
+elseif numlocalX == "7" then
+    g = 3
+
+elseif numlocalX == "8" then
+    h = 3
+
+elseif numlocalX == "9" then
+    i = 3
+
 end
 
+print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
+
+os.execute("clear")
+
+io.write("Agora O jogador Dois escolha um local para por seu número: ", "\n",
+print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
+)
 
 
+numlocalX = io.read()
+if numlocalX == "1" then
+    a = 6
+
+elseif numlocalX == "2" then
+    b = 6
+
+elseif numlocalX  == "3" then
+    c = 6
+
+elseif numlocalX == "4" then
+    d = 6
+
+elseif numlocalX == "5" then
+    e = 6
+
+elseif numlocalX == "6" then
+    f = 6
+
+elseif numlocalX == "7" then
+    g = 6
+
+elseif numlocalX == "8" then
+    h = 6
+
+elseif numlocalX == "9" then
+    i = 6
+
+ end
+
+ os.execute("clear")
+
+ io.write("Agora O jogador Um escolha um local para por seu número: ", "\n",
+print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
+)
+
+numlocalX = io.read()
+if numlocalX == "1" then
+    a = 3
+
+elseif numlocalX == "2" then
+    b = 3
+
+elseif numlocalX  == "3" then
+    c = 3
+
+elseif numlocalX == "4" then
+    d = 3
+
+elseif numlocalX == "5" then
+    e = 3
+
+elseif numlocalX == "6" then
+    f = 3
+
+elseif numlocalX == "7" then
+    g = 3
+
+elseif numlocalX == "8" then
+    h = 3
+
+elseif numlocalX == "9" then
+    i = 3
+
+end
+
+print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
+
+os.execute("clear")
+
+io.write("Agora O jogador Dois escolha um local para por seu número: ", "\n",
+print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
+)
+
+
+numlocalX = io.read()
+if numlocalX == "1" then
+    a = 6
+
+elseif numlocalX == "2" then
+    b = 6
+
+elseif numlocalX  == "3" then
+    c = 6
+
+elseif numlocalX == "4" then
+    d = 6
+
+elseif numlocalX == "5" then
+    e = 6
+
+elseif numlocalX == "6" then
+    f = 6
+
+elseif numlocalX == "7" then
+    g = 6
+
+elseif numlocalX == "8" then
+    h = 6
+
+elseif numlocalX == "9" then
+    i = 6
+
+ end
+
+ os.execute("clear")
+
+ print("Tic Tac Toe Game:", "\n", a, b, c, "\n", d, e, f,"\n", g, h, i )
 --[[learning about if then elseif to change the data on array
 
 print("digite um numero: ")
